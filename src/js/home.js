80c20c817e7059617488e0035ed9b7fcd7caaa8d
Vit Benton <viteksk8alot@gmail.com>
2018-12-13 14:25:26 -0500

    Various tweaks from base template

--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,5 @@
 $(document).ready(() => {
 
-    alert('Welcome to Node.js!');
 
-    $('.node-logo').mousedown(() => {
-        $('body').toggleClass('nodeGreen');
-    });
-
-    $('.node-logo').mouseup(() => {
-        $('body').toggleClass('nodeGreen');
-    });
 
 });
