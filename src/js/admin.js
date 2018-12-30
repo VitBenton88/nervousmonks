085de606cbcd58ac8fba12a40c4a3a407cd12bf1
Vit Benton <viteksk8alot@gmail.com>
2018-12-30 00:36:41 -0500

    Show edit form now propogates show details in input values

--- src/js/admin.js
+++ src/js/admin.js
@@ -55,11 +55,16 @@ $('.toolbar .button.edit').click( function() {
         date: $(this).data('date'),
         link: $(this).data('link')
     }
-
+    
     $('form.update .submit-show').attr('data-id', currentShow.id);
     $('#edit-modal .modal-content .location').html(currentShow.location);
     $('#edit-modal .modal-content .date').html(currentShow.date);
     $('#edit-modal .modal-content .link').html(currentShow.link);
+
+    //enter current show's details into appropriate inputs
+    $('#edit-modal .modal-content #show-location').val(currentShow.location);
+    $('#edit-modal .modal-content #show-date').val(new Date(currentShow.date).toISOString().split('T')[0]);
+    $('#edit-modal .modal-content #show-link').val(currentShow.link);
 
     $('#edit-modal').show();
 });
