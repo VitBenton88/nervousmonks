605eae197de7514a89f61e8d292a01fd743fdf67
Vit Benton <viteksk8alot@gmail.com>
2020-12-04 22:47:33 -0500

    Added live stream YT video iframe

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,12 @@
 $( document ).ready(function() {
-    const $youtube_iframe = $(".media.youtube iframe");
+    const $youtube_live_stream_iframe = $(".media.youtube.live-stream iframe");
+    const $youtube_music_video_iframe = $(".media.youtube.music-video iframe");
     const $soundcloud_iframe = $(".media.soundcloud iframe");
-    const youtube_iframe_src = "https://www.youtube.com/embed/9XjjFLAchb0";
+    const youtube_music_video_iframe_src = "https://www.youtube.com/embed/9XjjFLAchb0";
+    const youtube_live_stream_iframe_src = "https://www.youtube.com/embed/HPq0aY4MVtg";
     const soundcloud_iframe_src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/448310646&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true";
 
-    $youtube_iframe.attr('src', youtube_iframe_src);
+    $youtube_live_stream_iframe.attr('src', youtube_live_stream_iframe_src);
+    $youtube_music_video_iframe.attr('src', youtube_music_video_iframe_src);
     $soundcloud_iframe.attr('src', soundcloud_iframe_src);
 });
